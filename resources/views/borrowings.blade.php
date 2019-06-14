4fe83ce2ab3a478a535fd4f1b0dfb9bd7dc0522a
darkheart101 <kurokokoro@windowslive.com>
2019-06-14 07:43:42 +0300

    relationships

--- resources/views/borrowings.blade.php
+++ resources/views/borrowings.blade.php
@@ -7,9 +7,6 @@
 <table>
 @foreach ($borrowing_records as $borrowing_record)
 <tr>
-    {{-- {{ $borrowing_record->books_borrowed }} --}}
-    {{-- {{ $borrowing_record }} --}}
-    {{-- {{ $borrowing_record->borrow_date }} --}}
     <td>{{ $borrowing_record->borrow_date }}</td>
 
     @foreach ($borrowing_record->booksBorrowed as $book)
