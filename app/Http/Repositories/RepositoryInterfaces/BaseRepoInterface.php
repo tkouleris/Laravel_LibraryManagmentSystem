4f35313967230962b36f0ac0b1641650f075056f
darkheart101 <kurokokoro@windowslive.com>
2019-06-18 20:51:33 +0300

    Repository Interface fixes

--- app/Http/Repositories/RepositoryInterfaces/BaseRepoInterface.php
+++ app/Http/Repositories/RepositoryInterfaces/BaseRepoInterface.php
@@ -8,5 +8,6 @@
         public function get_record_by_id( $id );
         public function create_record( $data );
         public function update_record( $data );
+        public function delete_record_byID( $id );
 
     }
--- app/Http/Repositories/RepositoryInterfaces/BookRepoInterface.php
+++ app/Http/Repositories/RepositoryInterfaces/BookRepoInterface.php
@@ -5,5 +5,5 @@
     use App\Http\Repositories\RepositoryInterfaces\BaseRepoInterface;
 
     interface BookRepoInterface extends BaseRepoInterface{
-        public function delete_record_byID( $id );
+
     }
