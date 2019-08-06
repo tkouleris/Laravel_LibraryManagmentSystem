7c5707cd9ac2a7e70fdd5a839f327898a8096ac1
darkheart101 <kurokokoro@windowslive.com>
2019-08-06 06:02:19 +0300

    Login Test

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -5,9 +5,12 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class LoginTest extends TestCase
 {
+    use DatabaseTransactions;
+
     public function test_user_can_view_a_login_form()
     {
         $response = $this->get('/login');
@@ -17,9 +20,32 @@ class LoginTest extends TestCase
 
     public function test_user_cannot_view_a_login_form_when_authenticated()
     {
-        //$user = factory(\App\User::class)->make();
         $user = \App\User::find(1);
         $response = $this->actingAs($user)->get('/login');
         $response->assertRedirect('/dashboard');
+    }
+
+    public function test_user_can_login_with_correct_credentials()
+    {
+        $user = factory(\App\User::class)->create([
+            'password' => bcrypt($password = 'i-love-laravel'),
+        ]);
+
+        $response = $this->post('/login', [
+            'name' => $user->name,
+            'password' => $password,
+        ]);
+        $response->assertRedirect('/dashboard');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_user_cannot_login_with_wrong_credentials()
+    {
+        $response = $this->post('/login', [
+            'name' => 'no_user',
+            'password' => 'wrong_password',
+        ]);
+        $response->assertStatus(302);
+        $response->assertRedirect('/');
     }
 }
