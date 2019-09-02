2cdd55c56cbbf02ecd6a8b6f6d4e00eecfde3dff
darkheart101 <kurokokoro@windowslive.com>
2019-09-02 06:32:41 +0300

    Books Api index

---
+++ app/Http/Controllers/ApiControllers/BookController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\ApiControllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+use App\Http\Repositories\RepositoryInterfaces\BookRepoInterface;
+use App\Http\Repositories\RepositoryInterfaces\AuthorRepoInterface;
+
+class BookController extends Controller
+{
+    protected $bookRepo;
+    protected $authorRepo;
+
+    public function __construct(BookRepoInterface $book,
+                                AuthorRepoInterface $author){
+        $this->bookRepo = $book;
+        $this->authorRepo = $author;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = $this->bookRepo->get_all_limit_by();
+        return $books;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
