fa41391ad075020146ddcf888d375659fd49d495
darkheart101 <kurokokoro@windowslive.com>
2019-09-03 05:44:48 +0300

    minor problem fix

--- app/Http/Repositories/RepositoryInterfaces/BaseRepoInterface.php
+++ app/Http/Repositories/RepositoryInterfaces/BaseRepoInterface.php
@@ -4,7 +4,7 @@
 
     interface BaseRepoInterface{
 
-        public function get_all_limit_by( $limit );
+        public function get_all_limit_by( $limit = 0);
         public function get_record_by_id( $id );
         public function create_record( $data );
         public function update_record( $data );
