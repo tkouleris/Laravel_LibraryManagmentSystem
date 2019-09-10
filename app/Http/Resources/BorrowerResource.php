9404479e53c3597b9fbdea7176e7fd16ef2ebb75
darkheart101 <kurokokoro@windowslive.com>
2019-09-10 07:04:18 +0300

    borrower api format

--- app/Http/Resources/BorrowerResource.php
+++ app/Http/Resources/BorrowerResource.php
@@ -14,6 +14,16 @@ class BorrowerResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        //return parent::toArray($request);
+
+        return [
+            'id' => $this->id,
+            'firstname' => $this->firstname,
+            'lastname' => $this->lastname,
+            'dob' => $this->dob,
+            'address' => $this->address,
+            'phone' => $this->phone
+
+        ];
     }
 }
